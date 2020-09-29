07f24b86bb0616c7d9c9ad906100f69aa86779dd
Sachin Bhandari <69071243+SachinBhandari13@users.noreply.github.com>
2020-09-29 20:54:03 +0530

    Create pyhon.py

---
+++ pyhon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import cgi
+import subprocess as sp
+#print("location:https://www.google.com")
+print("content-type:text/html")
+print()
+
+cmd=cgi.FieldStorage().getvalue("x")
+
+if(("DATE" in cmd) or ("date" in cmd) or ("Date" in cmd) ):
+  out=sp.getoutput('date \'+Todays date is : %m %d %Y\' ')
+elif(("Cal" in cmd) or ("cal" in cmd) or ("calender" in cmd) or ("Calender" in cmd)):
+  out=sp.getoutput('cal')
+elif(("Time" in cmd) or ("time" in cmd) or ("Time" in cmd)):
+  out=sp.getoutput('date \'+Time is : %T\' ')
+
+print(out)
