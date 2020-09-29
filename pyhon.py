3bfa0f49b0d995f720dc75962786810581d2a434
Sachin Bhandari <69071243+SachinBhandari13@users.noreply.github.com>
2020-09-29 21:57:57 +0530

    Update pyhon.py

--- pyhon.py
+++ pyhon.py
@@ -11,7 +11,7 @@ if(("DATE" in cmd) or ("date" in cmd) or ("Date" in cmd) ):
   out=sp.getoutput('date \'+Todays date is : %m %d %Y\' ')
 elif(("Cal" in cmd) or ("cal" in cmd) or ("calender" in cmd) or ("Calender" in cmd)):
   out=sp.getoutput('cal')
-elif(("Time" in cmd) or ("time" in cmd) or ("Time" in cmd)):
+elif(("Time" in cmd) or ("time" in cmd) or ("TIME" in cmd)):
   out=sp.getoutput('date \'+Time is : %T\' ')
 
 print(out)
